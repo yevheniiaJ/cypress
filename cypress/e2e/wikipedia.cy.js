1f0ebcef9a0a601929408216292219f3ed0b5511
test tester <testert573@gmail.com>
2024-03-14 00:49:04 +0200

    test added

--- cypress/e2e/wikipedia.cy.js
+++ cypress/e2e/wikipedia.cy.js
@@ -1,10 +1,14 @@
 describe('template spec', () => {
   it('navigate to wikipedia', () => {
-    cy.visit('https://www.wikipedia.org/')
-    
+   cy.visit('https://www.wikipedia.org/')
   })
+
   it('click English', () => {
     cy.visit('https://www.wikipedia.org/')
     cy.get('#js-link-box-en').click();   
+    cy.get('.mw-headline').should('be.visible');
+    cy.get('#pt-login-2').should('be.visible');
+    cy.get('#pt-createaccount-2').should('be.visible');
+    cy.get('#vector-user-links-dropdown').should('be.visible');
   })
 })
